afded7eb656d4d0c77ad602ef292a0c5b85a648d
Vijay Bhatiya <vijaybhatiya007@gmail.com>
2023-10-09 15:32:52 +0530

    added envelope icon

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Envelope from "../Icons/Envelope";
 
 function Footer() {
   return (
@@ -12,10 +13,23 @@ function Footer() {
           <strong>Prompts</strong>forGood est une initiative de l'ONG Bayes
           Impact
         </div>
-        <div className="text-base lg:text-xl mt-6 lg:mt-8">Contact</div>
+        <div className="text-base lg:text-xl mt-6 lg:mt-8">
+          <Link
+            className="inline-flex items-center justify-center gap-2"
+            to="mailto:hello@bayesimpact.org"
+          >
+            <Envelope size={30} color="black" />
+            Contact
+          </Link>
+        </div>
         <div className="mt-6 lg:mt-8 mb-3 lg:mb-5">
-          <Link to="/terms-of-use" className="block md:inline-block">Conditions d'utilisation</Link> &{" "}
-          <Link to="/privacy-policy" className="block md:inline-block">Politique de confidentialité</Link>
+          <Link to="/terms-of-use" className="block md:inline-block">
+            Conditions d'utilisation
+          </Link>{" "}
+          &{" "}
+          <Link to="/privacy-policy" className="block md:inline-block">
+            Politique de confidentialité
+          </Link>
         </div>
         <div>© 2023 PromptsforGood, all right reserved.</div>
       </div>
